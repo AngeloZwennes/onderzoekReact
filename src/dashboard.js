8fc5b2e3dd96e6d0d962f17375f9521d01605ea0
Bodhie <stefanbode1995@hotmail.com>
2017-12-18 12:26:20 +0100

    File managemant
    Update different files

---
+++ src/dashboard.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './dashboard.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+
+class Dashboard extends React.Component {
+    render() {
+        return (
+            <div class="dashboard">
+                <div class="navbar">
+                    <h2 id="logo">FamilyCollab</h2>
+                    <div class="icons">
+                        <a class="fa fa-cog" id="settings"></a>
+                        <a class="fa fa-user" id="user"></a>
+                    </div>
+                </div>
+                <div class="container modulesspace">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <div class="col-sm-6 ">
+                                    <div class="box1 toprow">
+                                        <app-photo></app-photo>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6" >
+                                    <div class="box2 toprow">
+                                        <app-family></app-family>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6 ">
+                                    <div class="whitebox bottomrow">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6" >
+                                    <div class="whitebox bottomrow"><app-task></app-task></div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-sm-6 ">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="whitebox largebox">
+                                        <app-calendar></app-calendar>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Dashboard from './dashboard.js';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class Login extends React.Component {
@@ -21,62 +22,6 @@ class Login extends React.Component {
         );
     }
 }
-
-class Dashboard extends React.Component {
-    render() {
-        return (
-        <div class="dashboard">
-            <div class="navbar">
-                <h2 id="logo">FamilyCollab</h2>
-                <div class="icons">
-                    <a class="fa fa-cog" id="settings"></a>
-                    <a class="fa fa-user" id="user"></a>
-                </div>
-            </div>
-            <div class="container modulesspace">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="row">
-                            <div class="col-sm-6 ">
-                                <div class="box1 toprow">
-                                    <app-photo></app-photo>
-                                </div>
-                            </div>
-                            <div class="col-sm-6" >
-                                <div class="box2 toprow">
-                                    <app-family></app-family>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6 ">
-                                <div class="whitebox bottomrow">
-                                </div>
-                            </div>
-                            <div class="col-sm-6" >
-                                <div class="whitebox bottomrow"><app-task></app-task></div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="col-sm-6 ">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="whitebox largebox">
-                                    <app-calendar></app-calendar>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        );
-    }
-}
-
 
 ReactDOM.render(
     <Dashboard />,
